48bdc3341aff43f7cb866246a5f83a90ffdb83ab
samsontedla <samitedla23@gmail.com>
2021-06-17 13:06:39 -0700

    fixed errors on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,11 @@
 int main(void)
 
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
 
-	return (0);
+	char str[] = "\"Programming is like building a multilingual puzzle"; 
+
+	puts(str); 
+
+	return (0); 
 
 }
