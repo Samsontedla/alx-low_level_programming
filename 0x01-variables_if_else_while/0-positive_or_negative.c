897bfaa06e6eda88a5e84781b9e21ddde27d53dc
samsontedla <samitedla23@gmail.com>
2021-06-18 09:22:33 -0700

    print out positive or negative based on input n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,21 +7,19 @@
 int main(void)
 {
 		int n;
-
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					/* your code goes there */
-				if (n>0)
-				{
-					printf("is positive\n");
-				}
-				if (n<0)
-				{
-					printf("is negative\n");
-				}
-				if (n==0)
-				{
-					printf("is zero\n");
-				}
-					return (0);
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		if (n > 0)	
+		{
+			printf("%d is positive\n", n);
+		}
+		if (n < 0)
+		{
+			printf("%d is negative\n", n);	
+		}
+		if (n == 0)
+		{
+			printf("%d is zero\n", n);
+		}
+		return (0);
 }
