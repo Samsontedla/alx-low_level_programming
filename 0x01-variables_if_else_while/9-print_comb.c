f759f7bdab920a46d2190809f3ddd1db527e34da
samsontedla <samitedla23@gmail.com>
2021-06-20 08:10:44 -0700

    print combinations of numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ *main - Entry point
+ *
+ *return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+	putchar(n % 10 + '0');
+	n++;
+	if (n <= 9)
+		putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+return (0);
+}
