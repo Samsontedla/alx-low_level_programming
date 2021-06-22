c83e883f7187e2c0896fcab1df21727909925b38
samsontedla <samitedla23@gmail.com>
2021-06-22 10:15:53 -0700

    check if "c" is lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_islower - function that checks if c is lowercase
+ *
+ *Return: 1 if lower case, 0 otherwise
+ */
+int _islower(int c)
+{
+if (c <= 171 && c >= 141)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
