a84cbec5e099f91e6535aecadf2c5ad421a8ee00
samsontedla <samitedla23@gmail.com>
2021-06-23 06:54:31 -0700

    adds any two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *add - function that adds int i and j
+ *
+ *@i: any int i
+ *
+ *@j: any int j
+ *
+ * Return: result k
+ */
+int add(int i, int j)
+{
+int k;
+k = i + j;
+return (k);
+}
