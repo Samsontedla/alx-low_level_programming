b2c013ccfc57db5d462f2ffa2362744a03c0f5af
samsontedla <samitedla23@gmail.com>
2021-06-25 13:52:28 -0700

    function to print a diagonal line n times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_diagonal - function to print diagonal linbe
+ *
+ * @n: number of times
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (i == j)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
+}
