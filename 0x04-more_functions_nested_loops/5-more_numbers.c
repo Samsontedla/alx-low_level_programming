8e0c90272b7734713fe3a50be407167dfece68f6
samsontedla <samitedla23@gmail.com>
2021-06-25 13:35:42 -0700

    print numbers 1 - 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *more_numbers - print 0 - 14 10 x
+ *
+ * Return: Success
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
