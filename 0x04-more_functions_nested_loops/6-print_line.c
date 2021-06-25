00f9b010fd2920eff4b5b375f7237dd22dfab03f
samsontedla <samitedla23@gmail.com>
2021-06-25 13:43:58 -0700

    function to print out line on terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_line - draw a line in terminal
+ *
+ *@n: input integer
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
