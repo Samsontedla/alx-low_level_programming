44acf05791221d9a333822a8e232dda8ee42e580
samson tedla <samitedla23@gmail.com>
2021-06-29 08:40:29 +0000

    function that prints in reverese

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,35 @@
 #include "holberton.h"
 /**
+ *_strlen - function that prints length of string
+ *
+ *@s: string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
  *print_rev - function that prints a string in reverse
  *
  *@s: string
  *
  * Return: Success
  */
+
 void print_rev(char *s)
 {
-	int i, len = 68;
+	int len, i;
 
-	/**
-	 * while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	*/
+	len = _strlen(s);
+
 	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
