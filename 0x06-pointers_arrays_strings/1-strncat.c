648dd48232282946e3aabc1f1835e2067969e38f
samson tedla <samitedla23@gmail.com>
2021-07-01 12:38:39 +0000

    fixing error on 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,18 +19,20 @@ int _strlen(char *s)
  *_strncat - fuction that concatenates two string
  *@dest: destination string
  *@src: source string
- *@n: input n
+ *@n: most length
  *Return: destination
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i,
-
+	int i;
+	int len_d;
+	
 	len_d = _strlen(dest);
 
 	for (i = 0; i < n ; ++i, ++len_d)
 	{
 		dest[len_d] = src[i];
 	}
+	dest[len_d] = '\0';
 	return (dest);
 }
