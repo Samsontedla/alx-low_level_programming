7a6c91846a30afe852164cd2f402ec5d46279add
samson tedla <samitedla23@gmail.com>
2021-07-01 15:00:15 +0000

    function that capitalizes first letters of words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+/**
+ *cap_string - function that capitalizes first letter of every word
+ *@s: string
+ *Return: capitalized string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+				continue;
+			}
+		}
+		if (s[i] == '\t' || s[i] == ';' || s[i] == '.' || s[i] == '"')
+		{
+			i++;
+
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+				continue;
+			}
+		}
+		if (s[i] == ' ' || s[i] == '\n')
+		{
+			++i;
+
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+				continue;
+			}
+		}
+		else
+			continue;
+	}
+	return (s);
+}
