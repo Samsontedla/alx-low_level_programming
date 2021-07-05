64762b05f8996d493561df8f9ed10c4264178175
samson tedla <samitedla23@gmail.com>
2021-07-05 20:57:29 +0000

    strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k, m, count = 0;
+	
+	for (i = 0; needle[i] !='\0'; i++)
+	{
+		for (j = 0 ; haystack[j] != '\0'; j++)
+		{
+			if (haystack[j] == needle[0])
+			{
+				for (k = 0 ; k < i; k++)
+				{
+					if (haystack[j] == needle[k])
+					{
+						j++;
+						count++;
+					}
+					if (count == i)
+					{
+						for (m = j - i; haystack[m] != '\0'; m++)
+						{
+							return (haystack[m]);
+						}
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
