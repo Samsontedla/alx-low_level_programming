7bd155af5f9308ff3e1bb95e8f8292e2a8684611
samson tedla <samitedla23@gmail.com>
2021-07-06 18:06:58 +0000

    advanced tasks

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
  *set_string - function that sets the value of a pointer to a char
- *
- *
- *
+ *@s: string
+ *@to: second string
+ *return: nothing
  */
 void set_string(char **s, char *to)
 {
