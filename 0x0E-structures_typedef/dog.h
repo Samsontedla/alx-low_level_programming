db83239af890735a345a931179f5b4c9fc6a6ec1
samson tedla <samitedla23@gmail.com>
2021-07-19 20:30:42 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - description for dogs
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: name of owener
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
