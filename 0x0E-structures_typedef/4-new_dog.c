46c0f14fdc724b7469b8b5695ede0003f8023767
samson tedla <samitedla23@gmail.com>
2021-07-19 22:27:33 +0000

    add new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *new_dog - function that creates a new dog
+ *@name: name of new dog
+ *@age: age of new dog
+ *@owner: owner of new dog
+ *Return: new dog if success or NULL in error
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	unsigned int name_len = 0, owner_len = 0, i;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	dog = malloc(sizeof(dog_t));
+
+	while (*name++)
+		name_len++;
+	while (*owner++)
+		owner_len++;
+
+	dog->name = malloc(name_len * (sizeof(char)));
+
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < name_len; i++)
+	{
+		dog->name[i] = name[i];
+	}
+
+	dog->owner = malloc(owner_len * (sizeof(char)));
+
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < owner_len; i++)
+	{
+		dog->owner[i] = owner[i];
+	}
+	dog->age = age;
+	return (dog);
+}
