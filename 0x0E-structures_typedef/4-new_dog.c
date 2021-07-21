e37693e0c2ee60ed74d386d849053ee5c0d76df8
samson tedla <samitedla23@gmail.com>
2021-07-21 13:59:07 +0000

    print name

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,14 +17,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 
-	for (; name[name_len]; name_len++)
+	for (; name[name_len] != '\0'; name_len++)
 		;
 	name_len++;
-	for (; owner[owner_len]; owner_len++)
+	for (; owner[owner_len] != '\0'; owner_len++)
 		;
 	owner_len++;
 
-	dog->name = malloc((name_len * (sizeof(char))) + 1);
+	dog->name = malloc(name_len * (sizeof(char)));
 
 	if (dog->name == NULL)
 	{
@@ -35,7 +35,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		dog->name[i] = name[i];
 	}
-	dog->owner = malloc((owner_len * (sizeof(char))) + 1);
+	dog->owner = malloc(owner_len * (sizeof(char)));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
