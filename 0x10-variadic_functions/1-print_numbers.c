f20b773abbe7de0a165d7ad286b1fc71522c0c92
samson tedla <samitedla23@gmail.com>
2021-07-22 16:58:13 +0000

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (i == (n - 1))
+		if (i == (n - 1) || separator == NULL)
 			break;
 		printf("%s", separator);
 	}
