9146680fa8fa7b7e2869b89f8e5960f74ccb91f2
samson tedla <samitedla23@gmail.com>
2021-08-09 21:42:23 +0000

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,6 +12,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t op, rd, wr;
 	char *buffer;
 
+	if (filename == NULL)
+		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 
 	if (buffer == NULL)
