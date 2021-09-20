d333d5746c949d321015c081cdb955bf1bf675c4
samson tedla <samitedla23@gmail.com>
2021-09-20 16:10:11 +0000

    dynamic library

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *_str_len - functions that counts length of a string
+ *@s: string
+ *Return: length
+ */
+int str_len(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+/**
+ *_strcat - fuction that concatenates two string
+ *@dest: destination string
+ *@src: source string
+ *Return: destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i,
+
+	len_d = str_len(dest);
+
+	for (i = 0; src[i] != '\0'; ++i, ++len_d)
+	{
+		dest[len_d] = src[i];
+	}
+	return (dest);
+}
