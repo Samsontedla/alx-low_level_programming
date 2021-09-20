dd87b39540e22320d5a3c0c1ba8c5772829d7421
samson tedla <samitedla23@gmail.com>
2021-09-20 16:38:34 +0000

    script that creates a dynamic library

--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
- *_str_len - functions that counts length of a string
- *@s: string
- *Return: length
- */
-int str_len(char *s)
-{
-	int length = 0;
-
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-	return (length);
-}
-/**
  *_strcat - fuction that concatenates two string
  *@dest: destination string
  *@src: source string
@@ -25,7 +9,7 @@ char *_strcat(char *dest, char *src)
 {
 	int i,
 
-	len_d = str_len(dest);
+	len_d = _strlen(dest);
 
 	for (i = 0; src[i] != '\0'; ++i, ++len_d)
 	{
--- 0x18-dynamic_libraries/1-strncat.c
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
- *_strlen - functions that counts length of a string
- *@s: string
- *Return: length
- */
-int _str_len(char *s)
-{
-	int length = 0;
-
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-	return (length);
-}
-/**
  *_strncat - fuction that concatenates two string
  *@dest: destination string
  *@src: source string
@@ -27,7 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int len_d;
 
-	len_d = _str_len(dest);
+	len_d = _strlen(dest);
 
 	for (i = 0; src[i] != src[n] ; ++i, ++len_d)
 	{
